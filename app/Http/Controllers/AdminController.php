6dd2335d3522cecb4e2c1c5f59095d59b61a5add
iNd0n3s1A_cyb3R_Cr1M3 <djakarta1973@gmail.com>
2023-12-02 12:23:29 +0700

    add fill admin page, auth login

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+    public function loginHandler(Request $request){
+        return 'hello admin page';
+    }
 }
--- routes/admin.php
+++ routes/admin.php
@@ -7,6 +7,7 @@ Route::prefix('admin')->name('admin.')->group(function(){
 
     Route::middleware(['guest:admin'])->group(function(){
         Route::view('/login', 'back.pages.admin.auth.login')->name('login');
+        Route::post('/login_handler', [AdminController::class,'loginHandler'])->name('login_handler');
     });
 
     Route::middleware(['auth:admin'])->group(function(){
